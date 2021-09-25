4536d858bcc3ef2fe81ecd5f7a9f28fdb433a62c
Istiyaq123 <57604080+Istiyaq123@users.noreply.github.com>
2021-09-25 11:30:46 +0100

    Update page1.js

--- visualhub/page1.js
+++ visualhub/page1.js
@@ -496,7 +496,7 @@ function CarpentersPrimarySchool792(){
 function StratfordHighStStnCarpentersRd792(){
   topTextYes.style.display ="block";
   topTextNo.style.display ="none";
-  topTextYes.innerHTML = "Carpenters Primary School";
+  topTextYes.innerHTML = "Stratford High St Stn / CarpentersRd";
   BackDestination.setAttribute( "onClick", "CarpentersPrimarySchool792();" );
   NextDestination.setAttribute( "onClick", "StratfordBusStation792();" );
   topTextYes.style.animationName = "StratfordHighStStnCarpentersRd";
