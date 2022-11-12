b545b9a0eb4d7d06398ba85ea1bf62f136c095d0
Istiyaq123 <57604080+Istiyaq123@users.noreply.github.com>
2022-11-12 03:06:12 +0000

    Update newibus.js

--- IA/newibus.js
+++ IA/newibus.js
@@ -5,10 +5,58 @@ function BusTerminatesHerenewIbus(){
 function newOldStreetStation135btn(){
   newCrossharbourASDA135()
   newIbusdisplayRouteDesibus.innerHTML = "135 to Old Street, Provost Street";
+  var audio = 1;
+  var audio = 1;
+  iBusAudio.src="https://transportforlondon.sharepoint.com/sites/ic-68268-j1v9/Ibus/Audio%20announcements/IBus%20Announcements/R_135_001.mp3";
+  iBusAudio.play();
+  iBusAudio.onended = function() {
+    audio = 2;
+    if (audio === 2){
+      iBusAudio.src="https://transportforlondon.sharepoint.com/sites/ic-68268-j1v9/Ibus/Audio%20announcements/IBus%20Announcements/SA_To_001_V03.mp3";
+      iBusAudio.play();
+      iBusAudio.onended = function() {
+        audio = 3;
+        if (audio == 3){
+          iBusAudio.src="https://transportforlondon.sharepoint.com/sites/ic-68268-j1v9/Ibus/Audio%20announcements/IBus%20Announcements/D_OLD_STREET_001.mp3";
+          iBusAudio.play();
+          iBusAudio.onended = function() {
+            audio = 4;
+            if (audio == 4){
+              iBusAudio.src="https://transportforlondon.sharepoint.com/sites/ic-68268-j1v9/Ibus/Audio%20announcements/IBus%20Announcements/S_PROVOST_STREET_001.mp3";
+              iBusAudio.play();
+              iBusAudio.onended = function() {
+                iBusAudio.src="hhjff.mp3";
+                iBusAudio.play();
+            }
+          }
+        }
+      }
+    } 
+  }
 }
 function nenCrossharbour135btn(){
   newOldStreetStation135()
   newIbusdisplayRouteDesibus.innerHTML = "135 to Crossharbour, Asda";
+  iBusAudio.src="https://transportforlondon.sharepoint.com/sites/ic-68268-j1v9/Ibus/Audio%20announcements/IBus%20Announcements/R_135_001.mp3";
+  iBusAudio.play();
+  iBusAudio.onended = function() {
+    audio = 2;
+    if (audio === 2){
+      iBusAudio.src="https://transportforlondon.sharepoint.com/sites/ic-68268-j1v9/Ibus/Audio%20announcements/IBus%20Announcements/SA_To_001_V03.mp3";
+      iBusAudio.play();
+      iBusAudio.onended = function() {
+        audio = 3;
+        if (audio == 3){
+          iBusAudio.src="https://transportforlondon.sharepoint.com/sites/ic-68268-j1v9/Ibus/Audio%20announcements/IBus%20Announcements/D_Crossharbour_Asda_001.mp3";
+          iBusAudio.play();
+          iBusAudio.onended = function() {
+            iBusAudio.src="hhjff.mp3";
+            iBusAudio.play();
+          }
+        }
+      }
+    } 
+  };
 }
 
 
