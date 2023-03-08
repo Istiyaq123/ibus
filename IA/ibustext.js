9c6276904010995e6c36030f202eaf2f551c2ac2
Istiyaq123 <ahmedistiyaqqq03@gmail.com>
2023-03-08 14:13:14 +0000

    Milner Road

--- IA/ibustext.js
+++ IA/ibustext.js
@@ -16537,3 +16537,24 @@ function BeaumontRoadAudio(){
     iBusAudio.src=firstAudio+"Beaumont_Road"+lastAudio;
     iBusAudio.play();
 }
+function MilnerRoad(){
+    if (ibusView === "old"){
+        topTextYes.style.display ="none";
+        topTextNo.style.display ="block";
+        marquee.style.display = "none";
+        topTextNo.innerHTML = "Milner Road";
+    // topTextYes.style.animationName = "StGeorgesTownHallShadwellStn";
+    } else if (ibusView === "new"){
+        newIbusDisplayDesText.innerHTML = "Milner Road";
+        underground.style.display = "none";
+        overground.style.display = "none";
+        DLR.style.display = "none";
+        ElizabethLine.style.display = "none";
+        tfl_National_Rail_logo.style.display = "none";
+        tfl_trams_Roundel.style.display = "none";
+    }
+}
+function MilnerRoadAudio(){
+    iBusAudio.src=firstAudio+"Milner_Road"+lastAudio;
+    iBusAudio.play();
+}
